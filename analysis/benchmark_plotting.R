1a9e56366f74df66f3d55d1e30d499a8a169fb8e
Michael McAuliffe <michael.e.mcauliffe@gmail.com>
2025-07-17 13:14:43 -0700

    Clean up analysis scripts

--- analysis/benchmark_plotting.R
+++ analysis/benchmark_plotting.R
@@ -50,82 +50,34 @@ data$adaptation <- factor(data$adaptation, levels=c('none', 'base', 'manual', 'm
 plotData <- summarySE(data=data, measurevar = 'alignment_score', groupvars=c('evaluation'))
 baseplotData <- summarySE(data=data, measurevar = 'alignment_score', groupvars=c('language', 'adaptation'))
 
-t = speaker_subsets %>% subset(language == 'english' & adaptation == 'base' & speaker == 's37')
-t2 = speaker_subsets %>% subset(num_speakers == 34 & language == 'mandarin' & adaptation == 'base')
-
-t3 = t %>% group_by(utterance, num_speakers) %>% summarise(alignment_score=mean(alignment_score)) %>% group_by(utterance) %>% summarise(alignment_score=mean(alignment_score))
-t2 %>% group_by(speaker) %>% summarise(mean(alignment_score))
-
-ggplot(aes(x=num_speakers, y=mean * 1000, color=adaptation), data=speakerplotData) + geom_point(size = 5) +
-  geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5) +
-  ylab('Phone boundary error (ms)') + xlab('Number of adaptation speakers') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +facet_wrap(~language) + 
-  geom_hline(aes(yintercept=mean*1000, color=adaptation), data=baseplotData,size=2) + scale_color_manual(values=c('#D43610','#8338EC','#6EC200', "#FB5607")) #+ 
-  #geom_hline(aes(yintercept=mean*1000), data=subset(baseplotData, adaptation=='base'),color='#8338EC',size=2) + 
-  #geom_hline(aes(yintercept=mean*1000), data=subset(baseplotData, adaptation=='manual'),color='#6EC200',size=2)
-
-ggplot(aes(x=adaptation, y=mean * 1000), data=baseplotData) + geom_point(size = 5, color='#FB5607') +
-  geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5, color='#FB5607') +
-  ylab('Phone boundary error (ms)') + xlab('Alignment condition') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +
-  scale_x_discrete(guide = guide_axis(n.dodge = 2)) +facet_wrap(~language)
-
 baseplotData$language <- factor(baseplotData$language, levels=c('arpa', 'english','german', 'czech', 'mandarin'))
 
 
 
 ggplot(aes(x=adaptation, y=mean * 1000, color=language, group=language), data=baseplotData) + geom_point(size = 3) + geom_path(size=1) +
   #geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5) +
-  ylab('Phone boundary error (ms)') + xlab('Adaptation condition') +ggtitle('Phone boundary errors') +
+  ylab('Phone boundary error (ms)') + xlab('Adaptation condition') +#ggtitle('Phone boundary errors') +
   theme_memcauliffe_light() +
-  scale_x_discrete(, labels=c("None", 'Naive', 'Reference'))+ scale_color_manual(values=cbbPalette, name="Language", labels=c('English ARPA', "English", 'German', "Czech", 'Mandarin'))
+  scale_x_discrete(, labels=c("None", 'Base', 'Reference'))+ scale_color_manual(values=cbbPalette, name="Language", labels=c('English ARPA', "English", 'German', "Czech", 'Mandarin'))
+
+ggsave("case_study_three.png", width=1000, height=710, units="px", dpi=200)
 
 
+ggplot(aes(x=adaptation, y=mean * 1000, color=language, group=language), data=subset(baseplotData, language %in% c('arpa', 'english'))) + geom_point(size = 3) + geom_path(size=1) +
+  #geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5) +
+  ylab('Phone boundary error (ms)') + xlab('Adaptation condition') +#ggtitle('Phone boundary errors') +
+  theme_memcauliffe_light() +
+  scale_x_discrete(, labels=c("None", 'Base', 'Reference'))+ scale_color_manual(values=cbbPalette, name="Language", labels=c('English ARPA', "English", 'German', "Czech", 'Mandarin'))
 
-plotData <- summarySE(data=data, measurevar = 'phone_error_rate', groupvars=c('version', 'corpus','phone_set'))
-
-ggplot(aes(x=version, y=mean * 100), data=plotData) + geom_point(size = 5, color='#FB5607') +
-  geom_errorbar(aes(ymin = (mean - ci) * 100, ymax = (mean + ci)* 100),size=2, width=0.5, color='#FB5607') +
-  ylab('Phone error rate %') + xlab('Alignment condition') +ggtitle('Phone error rate') +
-  theme_memcauliffe() +
-  scale_x_discrete(guide = guide_axis(n.dodge = 2)) + facet_trelliscope(phone_set~corpus, ncol = 2, scales="free_x")
-
-
-labphon_data <- subset(data, corpus %in% c('timit', 'buckeye', 'csj', 'seoul') & evaluation %in% c('arpa_1.0', 'gp_1.0', 'mfa_3.0'))
-
-labphon_data[labphon_data$evaluation == 'gp_1.0',]$evaluation = 'arpa_1.0'
-labphon_data$evaluation <- factor(labphon_data$evaluation)
+ggsave("case_study_one.png", width=1000, height=710, units="px", dpi=200)
 
 
-plotData <- summarySE(data=labphon_data, measurevar = 'alignment_score', groupvars=c('evaluation', 'corpus'))
+baseplotData <- summarySE(data=data, measurevar = 'phone_error_rate', groupvars=c('language', 'adaptation'))
 
+baseplotData$language <- factor(baseplotData$language, levels=c('arpa', 'english','german', 'czech', 'mandarin'))
 
-ggplot(aes(x=evaluation, y=mean * 1000, color=corpus, group=corpus), data=plotData) +geom_path() + geom_point(size = 2.5)  +
-  ylab('Phone boundary error (ms)') + xlab('MFA version') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +
-  scale_x_discrete(labels=c("1.0", "3.0")) +
-  scale_color_manual(values=cbbPalette, labels=c('Buckeye', 'CSJ', "Seoul", "TIMIT"), name='Corpus')
-
-ggsave("docs/source/_static/benchmarks/phone_alignment.png", width=1000, height=800, units="px", dpi=200)
-
-
-uw_colloquium_data <- subset(data, corpus %in% c('timit', 'buckeye', 'csj', 'seoul') & evaluation %in% c('mfa_3.0'))
-uw_colloquium_data$corpus <- factor(uw_colloquium_data$corpu, levels=c('timit', 'buckeye', 'csj', 'seoul'))
-
-
-plotData <- summarySE(data=uw_colloquium_data, measurevar = 'alignment_score', groupvars=c('corpus'))
-
-
-ggplot(aes(x=corpus, y=mean * 1000), data=plotData) + geom_point(size = 6, color='#FB5607')  +
-  ylab('Phone boundary error (ms)') + xlab('Corpus') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +
-  scale_x_discrete(labels=c("English-TIMIT", 'English-Buckeye', 'Japanese-CSJ', "Korean-Seoul"))
-
-ggsave("docs/source/_static/benchmarks/uw_phone_alignment.png", width=1500, height=800, units="px", dpi=150)
-
-
-
-t <- subset(data, corpus=='seoul'& version=='3.0')
-
-
-subset(data, corpus=='buckeye' & phone_set=='mfa') %>% group_by(version) %>% summarise(n())
+ggplot(aes(x=adaptation, y=mean * 100, color=language, group=language), data=baseplotData) + geom_point(size = 3) + geom_path(size=1) +
+  #geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5) +
+  ylab('Phone error rate') + xlab('Adaptation condition') +#ggtitle('Phone boundary errors') +
+  theme_memcauliffe_light() +
+  scale_x_discrete(, labels=c("None", 'Base', 'Reference'))+ scale_color_manual(values=cbbPalette, name="Language", labels=c('English ARPA', "English", 'German', "Czech", 'Mandarin'))
--- analysis/varics_benchmark_plotting.R
+++ analysis/varics_benchmark_plotting.R
@@ -27,16 +27,15 @@ data$evaluation = factor(data$evaluation)
 
 
 data <- subset(data, !is.na(data$alignment_score))
-#data = subset(data, word_count > 1)
-#data = subset(data, !(word_count == 2 & reference_phone_count == 2))
 
 plotData <- summarySE(data=data, measurevar = 'alignment_score', groupvars=c('evaluation'))
 
-ggplot(aes(x=evaluation, y=mean * 1000), data=plotData) + geom_point(size = 5, color='#FB5607') +
+ggplot(aes(x=evaluation, y=mean * 1000), data=plotData) + geom_point(size = 5, color='#053561') +
   #geom_errorbar(aes(ymin = (mean - ci) * 1000, ymax = (mean + ci)* 1000),size=2, width=0.5, color='#FB5607') +
-  ylab('Phone boundary error (ms)') + xlab('Adaptation condition') +ggtitle('Phone boundary errors') +
+  ylab('Phone boundary error (ms)') + xlab('Adaptation condition') +#ggtitle('Phone boundary errors') +
   theme_memcauliffe_light() +
-  scale_x_discrete(guide = guide_axis(n.dodge = 2))
+  scale_x_discrete(labels=c('None', 'Base', 'Corrected', "Corrected+"))
+ggsave("static/case_study_two.png", width=1000, height=710, units="px", dpi=200)
 
 plotData <- summarySE(data=data, measurevar = 'phone_error_rate', groupvars=c('version', 'corpus','phone_set'))
 
@@ -46,42 +45,3 @@ ggplot(aes(x=version, y=mean * 100), data=plotData) + geom_point(size = 5, color
   theme_memcauliffe() +
   scale_x_discrete(guide = guide_axis(n.dodge = 2)) + facet_trelliscope(phone_set~corpus, ncol = 2, scales="free_x")
 
-
-labphon_data <- subset(data, corpus %in% c('timit', 'buckeye', 'csj', 'seoul') & evaluation %in% c('arpa_1.0', 'gp_1.0', 'mfa_3.0'))
-
-labphon_data[labphon_data$evaluation == 'gp_1.0',]$evaluation = 'arpa_1.0'
-labphon_data$evaluation <- factor(labphon_data$evaluation)
-
-
-plotData <- summarySE(data=labphon_data, measurevar = 'alignment_score', groupvars=c('evaluation', 'corpus'))
-
-
-ggplot(aes(x=evaluation, y=mean * 1000, color=corpus, group=corpus), data=plotData) +geom_path() + geom_point(size = 2.5)  +
-  ylab('Phone boundary error (ms)') + xlab('MFA version') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +
-  scale_x_discrete(labels=c("1.0", "3.0")) +
-  scale_color_manual(values=cbbPalette, labels=c('Buckeye', 'CSJ', "Seoul", "TIMIT"), name='Corpus')
-
-ggsave("docs/source/_static/benchmarks/phone_alignment.png", width=1000, height=800, units="px", dpi=200)
-
-
-uw_colloquium_data <- subset(data, corpus %in% c('timit', 'buckeye', 'csj', 'seoul') & evaluation %in% c('mfa_3.0'))
-uw_colloquium_data$corpus <- factor(uw_colloquium_data$corpu, levels=c('timit', 'buckeye', 'csj', 'seoul'))
-
-
-plotData <- summarySE(data=uw_colloquium_data, measurevar = 'alignment_score', groupvars=c('corpus'))
-
-
-ggplot(aes(x=corpus, y=mean * 1000), data=plotData) + geom_point(size = 6, color='#FB5607')  +
-  ylab('Phone boundary error (ms)') + xlab('Corpus') +ggtitle('Phone boundary errors') +
-  theme_memcauliffe() +
-  scale_x_discrete(labels=c("English-TIMIT", 'English-Buckeye', 'Japanese-CSJ', "Korean-Seoul"))
-
-ggsave("docs/source/_static/benchmarks/uw_phone_alignment.png", width=1500, height=800, units="px", dpi=150)
-
-
-
-t <- subset(data, corpus=='seoul'& version=='3.0')
-
-
-subset(data, corpus=='buckeye' & phone_set=='mfa') %>% group_by(version) %>% summarise(n())
